0adb7db8c7f865bbf9e05be32601f2c0101c5c94
tenglongroy <379600075@qq.com>
2017-11-08 16:21:45 +1100

    Update index.js
    create file lists of extensions and check conditions to go in different ajax call functions

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+var path = require('path');
 var express = require('express');
 var router = express.Router();
 
@@ -14,9 +15,28 @@ router.get('/show-path', function(req, res, next) {
   res.render('index', { currentFolder: 'qwetfgdfbk;n qeh oeh rar' });
 });
 
-/* for Ajax call, show specified path structure */
-router.get('/ajax-show-path', function(req, res, next) {
-  res.render('index', { currentFolder: 'qwetfgdfbk;n qeh oeh rar' });
+/* for Ajax call, return specified file's data */
+router.get('/ajax-show-file', function(req, res, next) {
+	var objectName = req.query.objectName;
+	var fileExtension = path.extname(objectName).toLowerCase();
+	var videoList = ['mp4', 'm4v', 'mpe', 'rmvb', 'rm', 'mkv', 'mov', 'wmv', 'flv', 'avi'];
+	var imageList = ['png', 'jpg', 'jpeg', 'jpe', 'bmp', 'tif', 'tiff', 'gif'];
+	var documentList = ['txt', 'pdf', ];
+	var audioList = [];
+	if(videoList.indexOf(fileExtension) >= 0){
+		getvideoAjax(req, res);
+	}
+	else if(imageList.indexOf(fileExtension) >= 0){
+		getFileAjax(req, res);
+	}
+	else if(documentList.indexOf(fileExtension) >= 0){
+		getFileAjax(req, res);
+	}
+	else{
+		getFileAjax(req, res);
+	}
+	next();
+  res.end();
 });
 
 module.exports = router;
