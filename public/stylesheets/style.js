a9d92308d29bc9dd062248d0611ade08f111bc73
tenglongroy <379600075@qq.com>
2017-11-09 17:47:11 +1100

    Update style.js

--- public/stylesheets/style.js
+++ public/stylesheets/style.js
@@ -1,16 +1,49 @@
+var videoList = ['mp4', 'm4v', 'mpe', 'rmvb', 'rm', 'mkv', 'mov', 'wmv', 'flv', 'avi'];
+var imageList = ['png', 'jpg', 'jpeg', 'jpe', 'bmp', 'tif', 'tiff', 'gif'];
+var documentList = ['txt', 'pdf' ];
+
 function constructNav(JSONFile){
     var pTag = document.createElement("p");
     pTag.setAttribute("name", JSONFile['name']);
     pTag.setAttribute("prefix", JSONFile['prefix']);
     pTag.setAttribute("level", JSONFile['level']);
-    var iconDiv = document.createElement("div");
-    pTag.prepend(iconDiv);
+    pTag.addClass('close');
+
+    var iconDiv = document.createElement("span");
+    pTag.appendChild(iconDiv);
+    var typeDiv = createTypeDiv(JSONFile['type']);
+    pTag.appendChild(typeDiv);
+
+    _constructNav(JSONFile['desc'], pTag);
+
+    return pTag;
 }
 
 
 function _constructNav(JSONFile, parentObject){
     var li = document.createElement("li")
+
+
+    parentObject.addClass(li);
 }
 
 
-//https://stackoverflow.com/questions/28256271/populate-ul-in-html-with-json-data
+
+function createTypeDiv(type){
+    var typeDiv = document.createElement("div");
+    if(type == 'folder'){
+        typeDiv.addClass('folder');
+    }
+    else if(videoList.indexOf(type) >= 0){
+        typeDiv.addClass('video');
+    }
+    else if(imageList.indexOf(type) >= 0){
+        typeDiv.addClass('image');
+    }
+    else if(documentList.indexOf(type) >= 0){
+        typeDiv.addClass('document');
+    }
+    else{
+        typeDiv.addClass('no-type');
+    }
+}
